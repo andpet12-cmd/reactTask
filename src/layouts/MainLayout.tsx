6215f74d236be5370f5afb2aecd0742c47148049
andpet12-cmd <andrpetr18912@gmail.com>
2025-01-06 21:52:37 +0200

    base route structure

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,9 @@
+import {Outlet} from "react-router";
+
+const MainLayout = () => {
+    return <>
+        Main layout
+        <Outlet/>
+    </>;
+}
+export default MainLayout;
---
+++ src/router/router.tsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router";
+import MainLayout from "../layouts/MainLayout.tsx";
+
+
+export const routes = createBrowserRouter([
+    {
+        path: "/", element: <MainLayout/>
+    }
+
+]);
