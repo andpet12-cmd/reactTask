0e34b3efb177b6ca2035c6c95544ad879b11d400
andpet12-cmd <andrpetr18912@gmail.com>
2025-01-06 11:48:27 +0200

    task2-1 Створення Todo, Todos

---
+++ src/components/Todos.tsx
@@ -0,0 +1,27 @@
+import {useEffect, useState} from "react";
+import {ITodo} from "../models/ITodo.ts";
+import {Todo} from "./todo/Todo.tsx";
+
+export const Todos = () => {
+
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+                    .then(value => value.json())
+                    .then((iTodos: ITodo[]) => {
+                        setTodos(iTodos);
+
+                     });
+
+
+
+    }, []);
+    return (
+        <>
+            {
+                todos.map(todo => <Todo key={todo.id} item={todo}/>)
+            }
+        </>
+    );
+};
