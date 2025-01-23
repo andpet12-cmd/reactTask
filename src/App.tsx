b72e4594c2c94c31f6c7b918d6b573d5822536d9
andpet12-cmd <andrpetr18912@gmail.com>
2025-01-24 01:46:39 +0200

    task5-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
+import {Outlet} from "react-router";
+import {Menu} from "./components/menu/Menu.tsx";
 
 function App() {
 
-  return (
-    <>
-        hello okten
-    </>
-  )
+    return (
+        <>
+            <Menu/>
+            <Outlet/>
+        </>
+    );
 }
 
 export default App
