6f187180b2968539b3da25a36f5f1df00a5de4cf
andpet12-cmd <andrpetr18912@gmail.com>
2025-01-28 17:05:05 +0200

    task7-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import './App.css'
-import {A} from "./components/A.tsx";
-import {B} from "./components/B.tsx";
-import {MyContext} from "./context/MyContextProvider.tsx";
-import {useState} from "react";
 
 function App() {
 
-    const [themeColor, setThemeColor] = useState('light');
-
     return (
         <div>
-            <MyContext.Provider value={{
-                theme: themeColor,
-                changeTheme: (themeValue: string) => {
-                    setThemeColor(themeValue);
-                }
-            }}>
-                <A/>
-                <B/>
-            </MyContext.Provider>
+
         </div>
     );
 }
