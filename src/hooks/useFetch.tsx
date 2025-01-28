f73674950642c96b6ad7c24fc10b026b22daf5dc
andpet12-cmd <andrpetr18912@gmail.com>
2025-01-28 18:13:53 +0200

    task7-2

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+
+export const useFetch = <T, >(url: string, defaultValue:T) => {
+
+    const [obj, setObj] = useState<T>(defaultValue)
+    useEffect(() => {
+        fetch(url)
+            .then(value => value.json())
+            .then(json => {
+                setObj(json)
+            });
+    }, []);
+
+    return obj;
+}
+
